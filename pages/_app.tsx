c558a43aae9ec36365c113e3f74b8a222f7261b5
David Rovani <david@rovani.net>
2022-07-26 10:39:58 -0500

    Migrate _app to TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
+import { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
-
-export default MyApp;
