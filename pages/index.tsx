28dff53782d711c7102fcf10d1e58e74ed256f02
David Rovani <david@rovani.net>
2022-07-26 11:19:11 -0500

    Added types for GetStaticProps and page constructor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import fs from "fs";
 import matter from "gray-matter";
-import { GetStaticProps } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import { ParsedUrlQuery } from "querystring";
 
-export const getStaticProps: GetStaticProps = async () => {
-  let posts: {
-    slug: string;
-    year: number;
-    fileName: string;
-    title: string;
-  }[] = [];
+interface Params extends ParsedUrlQuery {}
+
+export const getStaticProps: GetStaticProps<HomeProps, Params> = async (_) => {
+  let posts: HomeProps["posts"] = [];
 
   const yearFolders = fs.readdirSync("posts");
   yearFolders.forEach((year) => {
@@ -34,16 +32,16 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export default function Home({
-  posts,
-}: {
+interface HomeProps {
   posts: {
     slug: string;
     year: number;
     fileName: string;
     title: string;
   }[];
-}) {
+}
+
+const HomePage: NextPage<HomeProps> = ({ posts }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
       {posts.map(({ slug, year, title }) => (
@@ -60,4 +58,6 @@ export default function Home({
       ))}
     </div>
   );
-}
+};
+
+export default HomePage;
