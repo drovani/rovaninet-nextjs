6883e9fc079c57a46ff4fb32e46392519094c58b
David Rovani <david@rovani.net>
2023-02-01 14:18:44 -0600

    Delete useless hello api.

--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-const Hello = (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
-
-export default Hello
