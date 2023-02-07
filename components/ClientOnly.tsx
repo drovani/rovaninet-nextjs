aa76b60ffc52250f884cada23b4aebbba1e11713
David Rovani <david@rovani.net>
2023-02-07 11:23:59 -0600

    Create ClientOnly utility component.

---
+++ components/ClientOnly.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ClientOnly = ({ children, ...delegated }) => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+  return <div {...delegated}>{children}</div>;
+};
+
+export default ClientOnly;
