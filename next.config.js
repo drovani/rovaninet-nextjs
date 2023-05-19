734e325ad734409aabdb25a95ae6b8d83e4a5421
David Rovani <david@rovani.net>
2023-05-19 10:49:55 -0500

    Add robots.txt and sitemap.xml files

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
