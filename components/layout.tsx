e3c46916a3af8d1d12a125d16e814eae14a2ec55
drovani <david@rovani.net>
2025-02-17 21:32:59 -0600

    Fix lint errors.

--- components/layout.tsx
+++ components/layout.tsx
@@ -57,7 +57,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <main className="font-sans antialiased">
         {" "}
         <div className="rounded border p-2 bg-yellow-100">
-          If you like what you see (view the <a href="/about" className="underline decoration-from-font">About Me page</a> for more) and you're interested in
+          If you like what you see (view the <Link href="/about" className="underline decoration-from-font">About Me page</Link> for more) and you&apos;re interested in
           exploring opportunities for us to partner up, please reach out to me
           at <a href="mailto:david@rovani.net" className="decoration-dashed decoration-from-font underline">david@rovani.net</a> or message me on{" "}
           <a href="https://linkedin.com/in/drovani" className="underline decoration-from-font decoration-dashed">LinkedIn</a> #OpenToWork
