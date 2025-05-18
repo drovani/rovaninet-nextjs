6edbddbc522b91cd338804f5ae3ad317da97b066
drovani <david@rovani.net>
2025-05-18 16:16:28 -0500

    Replace custom generated sitemap component with next-sitemap library.

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://rovani.net',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './out',
+  changefreq: 'weekly'
+};
