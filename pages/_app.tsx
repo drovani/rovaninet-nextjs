0349ceaace0709cda8ff6bf071e0687ee988133b
drovani <david@rovani.net>
2025-11-05 16:40:56 -0600

    Add ability to render blog posts as slides for presentations. Still needs a lot of formatting work, though.,

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
   const headtitle = "Rovani's Sandbox";
+
+  // Check if current route is a talk page
+  const isTalkPage = router.pathname.startsWith('/talks/');
+
   return (
     <React.Fragment>
       <Head>
@@ -13,9 +19,13 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/images/r-star.png" />
       </Head>
-      <Layout>
+      {isTalkPage ? (
         <Component {...pageProps} />
-      </Layout>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </React.Fragment>
   );
 };
